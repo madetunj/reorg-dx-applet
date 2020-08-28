7a79f5e2cc0a3d2a29a934170e0159c1132cac8e
modupe adetunji <madetunj@stjude.org>
2020-08-28 10:45:42 -0500

    included instructions

--- testingoutput.py
+++ testingoutput.py
@@ -1,6 +1,5 @@
-import json
-
 import dxpy
+from datetime import datetime
 
 #@dxpy.entry_point('main')
 
@@ -8,39 +7,30 @@ def main():
     #reorg_conf___=None, reorg_status___=None):
 
     # download and parse `reorg_conf___`
-    conf_file = dxpy.DXFile('file-Fvy5zQ89pQvPXqJk86X6Bbbk')
-    #print(conf_file.get_id())
-
-    dxpy.download_dxfile(conf_file.get_id(), "conf.json")
-
-    with open('conf.json') as f:
-        conf = json.load(f)
+    analysis_id = "analysis-Fx2244Q95JGFQJYGFJXfQpYJ"
 
     # find the output stage of the current analysis
-
-    #job_id is reorg_app:main
-#    job = "job-FvqFQbQ9Q65yGKz0J2Kvkfz8"
-#    job = "job-FvqBp189Q65p0YG54GPgJXXP"
-#    job = "job-Fvy60409pQvBFG9Z6z0bx3Q7"
-#    analysis_id = dxpy.describe(job)["analysis"]
-
-    #analysis_id = "analysis-Fx00JJ09J273vqQb8P8B78z0" #SEASEQ-4-glob
-    analysis_id = "analysis-Fx02fK095JG3Pyb4BgQJj4Yz" #SEASEQ-5-motifs
-
-    #print(dxpy.describe(job))
-
-    #print(analysis_id, "analysis_id")
-
+    # analysis_id = dxpy.describe(job)["analysis"]
     stages = dxpy.describe(analysis_id)["stages"]
-
-#    print (stages,"stages")
 
     # retrieve the dictionary containing outputs, where key is the name of output and value is the link to the file.
     output_map = [x['execution']['output'] for x in stages if x['id'] == 'stage-outputs'][0]
     folder_location = [x['execution']['folder'] for x in stages if x['id'] == 'stage-outputs'][0]
 
+    # retrieve container id
     dx_container = dxpy.DXProject(dxpy.PROJECT_CONTEXT_ID)
+    
+    #create temporary folder to move all files in the output folder
+    DATESTAMP = datetime.today().strftime('%s')
+    temp_folder="/" + DATESTAMP + "-temp" 
+    dx_container.new_folder(temp_folder,parents=True)
+    for eachfile in (dx_container.list_folder(folder_location)['objects']):
+        dx_container.move(
+            destination=temp_folder,
+            objects=[ eachfile['id'] ]
+        ) 
 
+    # move required outputfiles to their preferred folders
 #FASTQC
     htmlfile = output_map['htmlfile']
     zipfile = output_map['zipfile']
@@ -49,7 +39,7 @@ def main():
 
     sample_name = dxpy.describe(htmlfile)["name"].split('_fastqc.html')[0]
 
-    folder = "/output_files/" + sample_name + "/QC_files/FASTQC"
+    folder = folder_location + "/" + sample_name + "/QC_files/FASTQC"
     dx_container.new_folder(folder,parents=True)
     for thestring in (htmlfile, zipfile, bam_htmlfile, bam_zipfile):
         if thestring is not None :
@@ -66,7 +56,7 @@ def main():
     qc_htmlfile = output_map['qc_htmlfile']
     qc_textfile = output_map['qc_textfile']
 
-    folder = "/output_files/" + sample_name + "/QC_files/STATS"
+    folder = folder_location + "/" + sample_name + "/QC_files/STATS"
     dx_container.new_folder(folder,parents=True)
     for thestring in (metrics_out, qc_statsfile, qc_htmlfile, qc_textfile):
         if thestring is not None :
@@ -74,9 +64,6 @@ def main():
                 destination=folder,
                 objects=[ thestring['$dnanexus_link'] ]
             )
-
-#FLANKBED
-    flankbedfile = output_map['flankbedfile']
 
 #BAMFILES
     sortedbam = output_map['sortedbam']
@@ -86,7 +73,7 @@ def main():
     bklist_indexbam = output_map['bklist_indexbam']
     mkdup_indexbam = output_map['mkdup_indexbam']
 
-    folder = "/output_files/" + sample_name + "/QC_files/STATS"
+    folder = folder_location + "/" + sample_name + "/BAM_files"
     dx_container.new_folder(folder,parents=True)
     for thestring in (sortedbam, mkdupbam, bklistbam, indexbam, bklist_indexbam, mkdup_indexbam):
         if thestring is not None :
@@ -109,7 +96,7 @@ def main():
     nm_summitsfile = output_map['nm_summitsfile']
     nm_wigfile = output_map['nm_wigfile']
 
-    prefix_peaks_folder = "/output_files/" + sample_name + "/PEAKS_files"
+    prefix_peaks_folder = folder_location + "/" + sample_name + "/PEAKS_files"
     folder = prefix_peaks_folder + "/NARROW_peaks"
     dx_container.new_folder(folder,parents=True)
     for thestring in (peakbedfile, peakxlsfile, summitsfile, wigfile, all_peakbedfile, all_peakxlsfile, all_summitsfile, all_wigfile, nm_peakbedfile, nm_peakxlsfile, nm_summitsfile, nm_wigfile):
@@ -147,25 +134,71 @@ def main():
     e_to_g_super_enhancers = output_map['e_to_g_super_enhancers']
     g_to_e_super_enhancers = output_map['g_to_e_super_enhancers']
 
-    folder = prefix_peaks_folder + "/STITCHED_REGIONS"
+    prefolder = prefix_peaks_folder + "/STITCHED_REGIONS"
+    folder = prefolder + '/gff'
     dx_container.new_folder(folder,parents=True)
-    for thestring in (pngfile, mapped_union, mapped_stitch, enhancers, super_enhancers, gff_file, gff_union, union_enhancers, stitch_enhancers, e_to_g_enhancers, e_to_g_super_enhancers, g_to_e_enhancers, g_to_e_super_enhancers):
+    for thestring in (gff_file, gff_union):
         if thestring is not None :
             dx_container.move(
                 destination=folder,
                 objects=[ thestring['$dnanexus_link'] ]
             )
+    folder = prefolder + '/mappedGFF'
+    dx_container.new_folder(folder,parents=True)
+    for thestring in (mapped_union, mapped_stitch):
+        if thestring is not None :
+            dx_container.move(
+                destination=folder,
+                objects=[ thestring['$dnanexus_link'] ]
+            )
+    for thestring in (pngfile, enhancers, super_enhancers, union_enhancers, stitch_enhancers, e_to_g_enhancers, e_to_g_super_enhancers, g_to_e_enhancers, g_to_e_super_enhancers):
+        if thestring is not None :
+            dx_container.move(
+                destination=prefolder,
+                objects=[ thestring['$dnanexus_link'] ]
+            )    
+        
+#FLANKBED
+    flankbedfile = output_map['flankbedfile']
 
 #MOTIFS
     ame_tsv = output_map['ame_tsv']
     ame_html = output_map['ame_html']
     ame_seq = output_map['ame_seq']
     meme = output_map['meme']
+    meme_summary = output_map['meme_summary']
     summit_ame_tsv = output_map['summit_ame_tsv']
     summit_ame_html = output_map['summit_ame_html']
     summit_ame_seq = output_map['summit_ame_seq']
     summit_meme = output_map['summit_meme']
+    summit_meme_summary = output_map['summit_meme_summary']
 
+    prefolder = folder_location + "/" + sample_name + "/MOTIF_files"
+    summit_motif_subfile = 'bklist' + (dxpy.describe(flankbedfile)["name"].split('bklist')[1]).split('.bed')[0]
+    motif_subfile = 'bklist' + (dxpy.describe(peakbedfile)["name"].split('bklist')[1]).split('.bed')[0]
+    folder = prefolder + "/" + motif_subfile + '-ame_out'
+    dx_container.new_folder(folder,parents=True)
+    for thestring in (ame_tsv, ame_html, ame_seq):
+        if thestring is not None :
+            dx_container.move(
+                destination=folder,
+                objects=[ thestring['$dnanexus_link'] ]
+            )
+    folder = prefolder + "/" + summit_motif_subfile + '-ame_out'
+    dx_container.new_folder(folder,parents=True)
+    for thestring in (summit_ame_tsv, summit_ame_html, summit_ame_seq):
+        if thestring is not None :
+            dx_container.move(
+                destination=folder,
+                objects=[ thestring['$dnanexus_link'] ]
+            )
+    #move flankbedfile and meme files to main MOTIF directory
+    for thestring in (flankbedfile, meme, summit_meme, meme_summary, summit_meme_summary):
+        dx_container.move(
+            destination=prefolder,
+            objects=[ thestring['$dnanexus_link'] ]
+        )
+        
 #BAM2GFF
     m_downstream = output_map['m_downstream']
     m_upstream = output_map['m_upstream']
@@ -174,10 +207,25 @@ def main():
     pdf_gene = output_map['pdf_gene']
     pdf_h_gene = output_map['pdf_h_gene']
     png_h_gene = output_map['png_h_gene']
-    pdf_h_gene = output_map['pdf_h_gene']
     pdf_promoters = output_map['pdf_promoters']
     pdf_h_promoters = output_map['pdf_h_promoters']
     png_h_promoters = output_map['png_h_promoters']
+
+    prefolder = folder_location + "/" + sample_name + "/BAMDensity_files"
+    folder = prefolder + "/matrix"
+    dx_container.new_folder(folder,parents=True)
+    for thestring in (m_downstream, m_upstream, m_genebody, m_promoters):
+        if thestring is not None :
+            dx_container.move(
+                destination=folder,
+                objects=[ thestring['$dnanexus_link'] ]
+            )
+    for thestring in (pdf_gene, pdf_h_gene, png_h_gene, pdf_promoters, pdf_h_promoters, png_h_promoters):
+        if thestring is not None :
+            dx_container.move(
+                destination=prefolder,
+                objects=[ thestring['$dnanexus_link'] ]
+            )
 
 #VISUALIZATION
     bigwig = output_map['bigwig']
@@ -190,42 +238,17 @@ def main():
     a_norm_wig = output_map['a_norm_wig']
     a_tdffile = output_map['a_tdffile']
 
+    folder = folder_location + "/" + sample_name + "/PEAKDisplay_files"
+    dx_container.new_folder(folder,parents=True)
+    for thestring in (bigwig, norm_wig, tdffile, a_bigwig, a_norm_wig, a_tdffile, n_bigwig, n_norm_wig, n_tdffile):
+        if thestring is not None :
+            dx_container.move(
+                destination=folder,
+                objects=[ thestring['$dnanexus_link'] ]
+            )
 
-
-    #print(dxpy.describe(norm_wig)["name"])
-
-#    folder = "/temporary"
-    # get the container instance
-#    dx_container = dxpy.DXProject(dxpy.PROJECT_CONTEXT_ID)
-
-#    dx_container.new_folder(folder,parents=True)
-#    for thegroup in output_map:
-#        thegroup_path = output_map[thegroup]
-#        #print(thegroup, thegroup_path)
-#        if thegroup_path is not None :
-#            dx_container.move(
-#                destination=folder,
-#                objects=[ thegroup_path['$dnanexus_link'] ]
-#            )
-
-#    dx_container.remove_folder(folder=folder_location, recurse=True)
-
-#    for ccc in dx_container.list_folder(folder_location)['objects']:
-#        print(ccc,ccc['id'])
-#        dx_container.remove_objects(ccc)
-#        print(ccc['id'])
-
-
-#    dx_container.new_folder(bam_folder,parents=True)
-#    dx_container.move(
-#        destination=vcf_folder,
-#        objects=[ vcf['$dnanexus_link'] ]
-#    )
-    #dx_container.move(
-     #   objects=bam_folder,
-      #  destination=[ bam['$dnanexus_link'] ],
-    #)
-
+    # remove the temporary folder created
+    dx_container.remove_folder(folder=temp_folder, recurse=True)
 
 if __name__ == "__main__":
     main()
